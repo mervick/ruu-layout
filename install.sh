1daa8f5b9be886253b8c21833382b9652b0adf6a
Andrey Izman <izmanw@gmail.com>
2020-02-13 04:06:52 +0200

    Add layouts in evdev.extras.xml

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ if [[ "$user" != 'root' ]]; then
   exit 1
 fi
 
+extra_file=/usr/share/X11/xkb/rules/evdev.extras.xml
 evdev_file=/usr/share/X11/xkb/rules/evdev.xml
 
 if [[ "$(cat $evdev_file | grep '<name>ruu</name>')" != "" ]]; then
@@ -20,9 +21,8 @@ fi
 
 regex='<layout>[\s\n]*<configItem>[\s\n]*<name>ru<\/name>[a-zA-Z0-9\s\n<>\/]*<\/configItem>[\s\n]*<variantList>'
 regex2="$(echo "$regex" | sed 's/\\s/[:space:]/g')"
-block="$(cat "$evdev_file" | grep -zoP "$regex")"
 
-if [[ "$block" == "" ]]; then
+if [[ "$(cat "$extra_file" | grep -zoP "$regex")" == "" || "$(cat "$evdev_file" | grep -zoP "$regex")" ]]; then
   (1>&2 echo 'Unable to find "ru" layout')
   exit 4
 fi
@@ -30,8 +30,12 @@ fi
 if [[ -f "$evdev_file.back" ]]; then
   mv "$evdev_file.back" "$evdev_file.back~$(date +%s)"
 fi
+if [[ -f "$extra_file.back" ]]; then
+  mv "$extra_file.back" "$extra_file.back~$(date +%s)"
+fi
 
 cp "$evdev_file" "$evdev_file.back"
+cp "$extra_file" "$extra_file.back"
 
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do
@@ -42,4 +46,6 @@ done
 DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
 
 variant="$(cat "$DIR/evdev.ruu-variant.xml" | tr '\n' '\f' | sed -s 's/\//\\\//g')"
+
 cat "$evdev_file" | tr '\n' '\f' | sed -E "s/($regex2)/\1$variant/" | tr '\f' '\n' > "$evdev_file"
+cat "$extra_file" | tr '\n' '\f' | sed -E "s/($regex2)/\1$variant/" | tr '\f' '\n' > "$extra_file"
