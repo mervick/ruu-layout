2066661e04511433a07d4e5d3c67f57339931c6a
Andrey Izman <izmanw@gmail.com>
2020-02-13 04:22:12 +0200

    Update uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -6,12 +6,19 @@ if [[ "$user" != 'root' ]]; then
   exit 1
 fi
 
+extra_file=/usr/share/X11/xkb/rules/evdev.extras.xml
 evdev_file=/usr/share/X11/xkb/rules/evdev.xml
 
-if [[ -f "$evdev_file.back" ]]; then
-  cp "$evdev_file.back" "$evdev_file"
-  exit 0
+if [[ ! -f "$evdev_file.back" ]]; then
+  (1>&2 echo "Failure. Unable to locate $evdev_file.back file")
+  exit 2
 fi
 
-(1>&2 echo "Failure. Unable to locate $evdev_file.back file")
-exit 2
+if [[ ! -f "$extra_file.back" ]]; then
+  (1>&2 echo "Failure. Unable to locate $extra_file.back file")
+  exit 3
+fi
+
+
+cp "$evdev_file.back" "$evdev_file"
+cp "$extra_file.back" "$extra_file"
