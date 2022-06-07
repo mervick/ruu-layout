c3d8c8eb5127ae589f606b7296c423847398319c
Andrey Izman <izmanw@gmail.com>
2022-06-08 01:55:22 +0300

    Add root access

--- install.py
+++ install.py
@@ -7,19 +7,17 @@ user = subprocess.run(['whoami'],
     stdout=subprocess.PIPE, stderr=subprocess.PIPE,
     check=True, text=True).stdout
 
-# if user != "root":
-#   print('Root permissions is required. Run it with sudo')
-#   exit(1)
+if user != "root":
+  print('Root permissions is required. Run it with sudo')
+  exit(1)
 
 
 class CommentedTreeBuilder(ElementTree.TreeBuilder):
-# This class will retain remarks and comments opposed to the xml parser default
     def comment(self, data):
         self.start(ElementTree.Comment, {})
         self.data(data)
         self.end(ElementTree.Comment)
 
-# the missing part:
 def parse_xml_with_remarks(filepath):
     ctb = CommentedTreeBuilder()
     xp = ET.XMLParser(target=ctb)
@@ -28,10 +26,8 @@ def parse_xml_with_remarks(filepath):
 
 test_file = './test/evdev.xml'
 ruu_variant = './evdev.ruu-variant.xml'
-# extra_file = '/usr/share/X11/xkb/rules/evdev.extras.xml'
 evdev_file = '/usr/share/X11/xkb/rules/evdev.xml'
 out_file = evdev_file
-# evdev_file = out_file
 
 contents = open(evdev_file).read()
 header_end = contents.find('<xkbConfigRegistry')
@@ -46,7 +42,6 @@ layouts = layoutList.findall('layout')
 
 for layout in layouts:
   configItem = layout.find('./configItem')
-# for configItem, variantList in layouts:
   if configItem.find('name').text == "ru":
     variantList = layout.find('./variantList')
     variants = variantList.findall('variant')
@@ -56,7 +51,6 @@ for layout in layouts:
         exit(5)
     variantList.append(ET.parse(ruu_variant).getroot())
 
-# subprocess.call("cp " + extra_file + " " + extra_file + ".back", shell=True)
 subprocess.call("cp " + evdev_file + " " + evdev_file + ".back", shell=True)
 
 with open(out_file, 'wb') as f:
